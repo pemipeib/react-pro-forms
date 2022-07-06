b9693effe53c261a575b0287af5d68e74aa27305
pemipeib <pemipeib@msn.com>
2022-07-06 18:59:33 +0200

    Inicio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+import {Navigation} from "./routes/Navigation";
+
+function App() {
+    return (
+        <>
+            <Navigation />
+
+        </>
+    );
+}
+
+export default App;
---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,35 @@
+import {BrowserRouter, Navigate} from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+import logo from "../logo.svg"
+
+export const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <div className="main-layout">
+                <nav>
+                    <img src={logo} alt="Logo" />
+                    <ul>
+                        <li>
+                            <NavLink to="/home" className={({isActive}) => isActive ? "nav-active" : ""}>Shopping</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className={({isActive}) => isActive ? "nav-active" : ""}>About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/users" className={({isActive}) => isActive ? "nav-active" : ""}>Users</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="home" element={<h1>Home</h1>} />
+                    <Route path="about" element={<h1>About Page</h1>} />
+                    <Route path="users" element={<h1>Users Page</h1>} />
+                    <Route path="/*" element={<Navigate to="home" replace />}/>
+                </Routes>
+
+            </div>
+        </BrowserRouter>
+    );
+};
